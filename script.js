3d6491496ed6d744367345e838f91020a99e50bc
vishnupri912 <kvishnupri12@gmail.com>
2025-07-10 13:25:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showHome(){
+    let doc=document.getElementById("home");
+    doc.style.display="block";
+}
+function showAbout(){
+    let ab=document.getElementById("about");
+    ab.style.display="block";
+}
